adda2b8b41c7f3f5dbeafe2365c6e2845d027adf
Nicolas Carrier <nicolas.carrier@parrot.com>
2016-07-11 23:17:55 +0200

    alchemy: build support for bebop and bebop 2
    Change-Id: I07caf5e906563352a37c8ebd8235d7dd9461d3ac

---
+++ Tools/Parrot/atom.mk
@@ -0,0 +1,86 @@
+LOCAL_PATH := $(call my-dir)
+
+APM_COMMON_CATEGORY_PATH := apm
+
+APM_COMMON_SRC_LIBRARY_DIRS := \
+	$(shell cat $(LOCAL_PATH)/../../Tools/ardupilotwaf/directories_list) \
+	AP_HAL_Linux \
+	AP_HAL/utility
+
+APM_COMMON_CXXFLAGS := \
+	-D_GNU_SOURCE \
+	-DCONFIG_HAL_BOARD=HAL_BOARD_LINUX \
+	-DF_CPU= \
+	-DGIT_VERSION=\"$(shell cd $(LOCAL_PATH); git describe --always --dirty)\" \
+	-DHAVE_CMATH_ISFINITE \
+	-DMAVLINK_PROTOCOL_VERSION=2 \
+	-DNEED_CMATH_ISFINITE_STD_NAMESPACE \
+	-DSKETCHBOOK="\"$(LOCAL_PATH)\"" \
+	-fdata-sections \
+	-ffunction-sections \
+	-fno-exceptions \
+	-fsigned-char \
+	-std=gnu++11 \
+	-Wno-missing-field-initializers \
+	-Wno-overloaded-virtual \
+	-Wno-reorder \
+	-Wno-unknown-pragmas \
+	-Wno-unknown-warning-option \
+	-Wno-unused-parameter
+
+APM_COMMON_C_INCLUDES := \
+	$(LOCAL_PATH)/../../libraries/AP_Common/missing \
+	$(LOCAL_PATH)/../../libraries/
+
+APM_COMMON_LIBRARIES := \
+	apm-mavlink-ardupilotmega \
+	libiio
+
+###############################################################################
+# ArduCopter
+###############################################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := arducopter
+LOCAL_MODULE_FILENAME := arducopter
+LOCAL_DESCRIPTION := ArduCopter is an open source autopilot
+LOCAL_CATEGORY_PATH := $(APM_COMMON_CATEGORY_PATH)
+
+$(call load-config)
+
+LOCAL_CONFIG_FILES := Config.in
+
+APM_COPTER_BEBOP_SRC_LIBRARY_DIRS := \
+	$(APM_COMMON_SRC_LIBRARY_DIRS) \
+	$(shell cat $(LOCAL_PATH)/../../ArduCopter/directories_list)
+
+APM_COPTER_BEBOP_SRC_DIRS := \
+	ArduCopter \
+	$(addprefix libraries/,$(APM_COPTER_BEBOP_SRC_LIBRARY_DIRS))
+
+LOCAL_SRC_FILES := \
+	$(foreach dir,$(APM_COPTER_BEBOP_SRC_DIRS),$(call all-cpp-files-in,../../$(dir)))
+
+LOCAL_COPY_FILES = 50-arducopter.rc:etc/boxinit.d/
+ifeq ($(CONFIG_ARDUPILOT_MILOS),y)
+LOCAL_COPY_FILES += ../Frame_params/Parrot_Bebop2.param:etc/arducopter/bebop.parm
+else
+LOCAL_COPY_FILES += ../Frame_params/Parrot_Bebop.param:etc/arducopter/bebop.parm
+endif
+
+LOCAL_CXXFLAGS := \
+	$(APM_COMMON_CXXFLAGS) \
+	-DSKETCH=\"ArduCopter\" \
+	-DSKETCHNAME="\"ArduCopter\"" \
+	-DAPM_BUILD_DIRECTORY=APM_BUILD_ArduCopter \
+	-DCONFIG_HAL_BOARD_SUBTYPE=HAL_BOARD_SUBTYPE_LINUX_BEBOP
+
+LOCAL_C_INCLUDES := \
+	$(APM_COMMON_C_INCLUDES)
+
+LOCAL_LIBRARIES := \
+	$(APM_COMMON_LIBRARIES)
+
+include $(BUILD_EXECUTABLE)
+
